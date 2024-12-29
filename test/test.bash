93a6be8683cee7d3547a270aa8519c457c47cafd
Haruhiko Kawai <mertris@outlook.jp>
2024-12-29 22:20:46 +0900

    change2 test.bash

--- test/test.bash
+++ test/test.bash
@@ -6,9 +6,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 180 ros2 launch mypkg talk_listen.launch.py 
+timeout 10 ros2 launch mypkg talk_listen.launch.py 
 
-#cat /tmp/mypkg.log |
-#grep 'Listen: 10'
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
 
 
